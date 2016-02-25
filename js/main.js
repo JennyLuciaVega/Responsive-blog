cc5d963936019ef70f3c0cb467d30562e717b94d
Jenny Lucia Vega <Jenny Lucia Vega>
2016-02-25 12:25:22 -0500

    delete

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var heroHeight = $('.text-head').outerHeight();
-function parallax(){
-    var scrolled = $(window).scrollTop();
-    $('.bg-hero').css('height', (heroHeight-scrolled) + 'px');
-}
-
-$(window).scroll(function(e){
-    parallax();
-});
